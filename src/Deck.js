0e26e6d6ceebc3b93f0640a9a0041dbdd1b2b7fb
Pedro Reis <pedroreisv@gmail.com>
2017-04-13 14:28:28 -0300

    Return card to default position on release

--- src/Deck.js
+++ src/Deck.js
@@ -19,11 +19,17 @@ class Deck extends Component {
         position.setValue({ x: gesture.dx, y: gesture.dy })
       },
       onPanResponderRelease: () => {
-
+        this.resetPosition()
       }
     })
 
     this.state = { panResponder, position }
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start()
   }
 
   getCardStyle() {
